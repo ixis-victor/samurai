5f9ad7759238a3405e53d9bfb1da1a4b4ad0b659
Matthew Proffitt <matthew.proffitt@ixis.co.uk>
2015-07-27 15:24:46 +0000

    Updating template.php to allow Materialize

--- themes/metallurgy_samurai/template.php
+++ themes/metallurgy_samurai/template.php
@@ -20,3 +20,29 @@ function metallurgy_samurai_theme($existing, $type, $theme, $path) {
     ),
   );
 }
+?>
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="sites/samurai/libraries/materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="sites/samurai/libraries/materialize/js/materialize.min.js"></script>
+    </body>
+  </html>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+    <a class="btn-floating btn-large red">
+      <i onclick="location.href='site/config/add'" class="large material-icons">add</i>
+    </a>
+    <ul>
+      <li><a class="btn-floating green"><i onclick="location.href='site'" class="material-icons">code</i></i></a></li>
+    </ul>
+  </div>
