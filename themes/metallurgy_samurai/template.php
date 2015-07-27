db9c7b30e24e9ebf9f74557376c941b82363fc12
Matthew Proffitt <matthew.proffitt@ixis.co.uk>
2015-07-27 15:46:49 +0000

    Updating library location

--- themes/metallurgy_samurai/template.php
+++ themes/metallurgy_samurai/template.php
@@ -25,7 +25,7 @@ function metallurgy_samurai_theme($existing, $type, $theme, $path) {
   <html>
     <head>
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="sites/samurai/libraries/materialize/css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="sites/all/libraries/materialize/css/materialize.min.css"  media="screen,projection"/>
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -34,7 +34,7 @@ function metallurgy_samurai_theme($existing, $type, $theme, $path) {
     <body>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="sites/samurai/libraries/materialize/js/materialize.min.js"></script>
+      <script type="text/javascript" src="sites/all/libraries/materialize/js/materialize.min.js"></script>
     </body>
   </html>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
