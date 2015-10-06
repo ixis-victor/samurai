6f53d23eff2ae8fb69953ca928b149459a2533a0
ixis-victor <Viktor.tamas@ixis.co.uk>
2015-10-06 09:37:36 +0100

    Create metallurgy_samurai_progress_bar.php

---
+++ profiles/samurai/themes/metallurgy_samurai/templates/system/metallurgy_samurai_progress_bar.php
@@ -0,0 +1,14 @@
+/**
+ * Returns HTML for a progress bar.
+ *
+ * Note that the core Batch API uses this only for non-JavaScript batch jobs.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - percent: The percentage of the progress.
+ *   - message: A string containing information to be displayed.
+ */
+
+<div class="progress">
+  <div class="indeterminate"></div>
+</div>
